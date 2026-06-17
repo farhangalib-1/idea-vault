0378d9b660357cd943cd5ce9dae381e1bd80ee2b
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-06-18 04:15:20 +0600

    Single Data is implemented

--- src/app/ideas/[Id]/page.jsx
+++ src/app/ideas/[Id]/page.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
-const page = () => {
+import {Chip} from "@heroui/react";
+import Image from 'next/image';
+const page = async({ params }) => {
+    const {Id} = await params;
+    const res = await fetch(`http://localhost:5000/allcollections/${Id}`);
+    const data = await res.json();
+    
   return (
     <div>
-      <h1>Page Details comming soon</h1>
+      <div className='border p-5 rounded-lg shadow-md bg-white/30 backdrop-blur-xl w-5/12 mx-auto my-10'>
+        <Image src={data.imageURL} alt={data.ideaTitle} width={800} height={400} className='rounded-lg object-cover mb-4 w-full' />
+        <Chip color="accent">{data.category}</Chip>
+        <h1 className='text-4xl font-bold'>{data.ideaTitle}</h1>
+        <p className='text-gray-600 my-1.5'>{data.detailedDescription}</p>
+        {
+            data.tags.map((tag, index) => <Chip key={index} className='mr-4' color="primary">{tag}</Chip>)
+        }
+        <div className="flex gap-3 mt-4">
+            <div className="border p-4 rounded-lg">
+        <p className='text-gray-600 my-1.5'> <span className='font-bold font-black'>Target Audience: </span>{data.targetAudience}</p>
+        <p className='text-gray-600 my-1.5'> <span className='font-bold font-black'>Estimated Budget: $</span>{data.estimatedBudget}</p>
+        </div>
+        <div className="border p-4 rounded-lg">
+        <p className='text-gray-600 my-1.5'> <span className='font-bold'>Problem Statement: </span>{data.problemStatement}</p>
+        <p className='text-gray-600 my-1.5'> <span className='font-bold'>Proposed Solution: </span>{data.proposedSolution}</p>
+        </div>
+        </div>
+      </div>
+      <div className='border p-5 rounded-lg shadow-md bg-white/30 backdrop-blur-xl w-5/12 mx-auto my-10'>
+        <h1 className='text-2xl font-bold mb-4'>Comments</h1>
+      </div>
     </div>
   )
 }
--- src/components/IdeaCard.jsx
+++ src/components/IdeaCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, Chip } from '@heroui/react';
 import Image from 'next/image';
+import Link from 'next/link';
 const IdeaCard = ({ idea }) => {
   return (
     <div className='border p-5 rounded-lg shadow-md bg-white/30 backdrop-blur-xl'>
@@ -8,7 +9,7 @@ const IdeaCard = ({ idea }) => {
     <Chip color="accent">{idea.category}</Chip>
       <h3 className='text-xl font-bold'>{idea.ideaTitle}</h3>
       <p className='text-gray-600'>{idea.shortDescription}</p>
-      <Button variant='primary' color='brand' className='mt-4 w-full'>View Details</Button>    
+      <Link href={`/ideas/${idea._id}`}><Button variant='primary' color='brand' className='mt-4 w-full'>View Details</Button></Link>
     </div>
   )
 }
