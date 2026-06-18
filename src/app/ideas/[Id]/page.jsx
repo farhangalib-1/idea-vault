582f64498d352be5a16961114a96f6c8f88ceda5
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-06-18 06:56:44 +0600

    Comment Section is updated

--- src/app/ideas/[Id]/page.jsx
+++ src/app/ideas/[Id]/page.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import {Button, Chip} from "@heroui/react";
+import {Button, Chip, Form} from "@heroui/react";
 import {Description, Label, TextArea, TextField} from "@heroui/react";
 import Image from 'next/image';
+
 const page = async({ params }) => {
     const {Id} = await params;
     const res = await fetch(`http://localhost:5000/allcollections/${Id}`);
     const data = await res.json();
+    const comment = await fetch("http://localhost:5000/comments", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:  JSON.stringify()
+    });
+
     
   return (
     <div>
@@ -30,14 +39,16 @@ const page = async({ params }) => {
       </div>
       <div className='border p-5 rounded-lg shadow-md bg-white/30 backdrop-blur-xl w-11/12  md:w-5/12 mx-auto my-10'>
         <h1 className='text-2xl font-bold mb-4'>Comments</h1>
+        <Form >
          <TextField className="w-full" name="message">
       <Label>your comment</Label>
-      <TextArea placeholder="Write your message here..." rows={4} />
+      <TextArea placeholder="Write your comment here..." rows={4} />
       <Description>Maximum 500 characters</Description>
     </TextField>
-    <Button className="mt-4" color="primary">
+    <Button type='submit'  className="mt-4" color="primary">
       Post Comment
     </Button>
+    </Form> 
       </div>
     </div>
   )
