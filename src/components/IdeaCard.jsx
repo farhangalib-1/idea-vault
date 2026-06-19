848ff435f554bd78c8219467228fd6cc10222140
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-06-19 23:58:47 +0600

    Image part is updated

--- src/components/IdeaCard.jsx
+++ src/components/IdeaCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Button, Chip } from '@heroui/react';
-import Image from 'next/image';
+
 import Link from 'next/link';
 const IdeaCard = ({ idea }) => {
   return (
     <div className='border p-5 rounded-lg shadow-md bg-white/30 backdrop-blur-xl'>
-    <Image src={idea.imageURL} alt={idea.ideaTitle} width={400} height={200} className='rounded-lg object-cover mb-4 w-full' />
+    <img src={idea.imageURL} alt={idea.ideaTitle} className='rounded-lg object-cover mb-4 w-full' />
     <Chip color="accent">{idea.category}</Chip>
       <h3 className='text-xl font-bold'>{idea.ideaTitle}</h3>
       <p className='text-gray-600'>{idea.shortDescription}</p>
